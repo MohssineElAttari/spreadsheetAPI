c69d9615b7b98ef6e8e67059aeeb91d2b5bfd257
Mohssine Elattari <elattari.mohssine@gmail.com>
2021-10-22 15:28:44 +0100

    new update migrations users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,14 +16,15 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('google_id');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
-            $table->string('access_token');
-            $table->integer('expires_in');
             $table->string('avatar');
+            $table->string('google_id');
+            $table->string('refresh_token')->nullable();;
+            $table->string('token')->nullable();
+            $table->integer('expires_in')->nullable();
+            $table->rememberToken();
+            $table->timestamp('email_verified_at')->nullable();
             $table->timestamps();
         });
     }
