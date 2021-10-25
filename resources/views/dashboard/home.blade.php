da6d16d586c6f12e0794622d2e580436af4266d7
Mohssine Elattari <elattari.mohssine@gmail.com>
2021-10-25 23:04:32 +0100

    add views dashboard

---
+++ resources/views/dashboard/home.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.dashboard')
+
+@section('content')
+
+    <div class="row my-5">
+        <h2 class="fs-4 mb-3">Create new API</h2>
+        <form class="row g-3" action="{{ route('readSheet') }}" method="post">
+            @csrf
+            <h3>Google Spreadsheet URL:</h3>
+            <div class="col-auto">
+                <p>Paste google spreadsheet URL here from address bar.
+                </p>
+            </div>
+            <div class="col">
+                <label for="inputPassword2" class="visually-hidden">Link :</label>
+                <input type="text" class="form-control" id="inputPassword2" placeholder="https://docs.google.com/spreadsheets/d/1JR2uAjnN67c4sRnfnyGXdzXjz535v6MNgB48pLvVI1I/edit#gid=0">
+            </div>
+            <button type="submit" class="btn btn-primary col-auto">CREATE API</button>
+        </form>
+    </div>
+@endsection
