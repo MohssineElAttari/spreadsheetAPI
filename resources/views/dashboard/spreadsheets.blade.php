7ef961c1521e775dc9c9f1ecd40acb8cb7210350
Mohssine Elattari <elattari.mohssine@gmail.com>
2021-11-03 15:33:35 +0100

    new update views spreadsheets

--- resources/views/dashboard/spreadsheets.blade.php
+++ resources/views/dashboard/spreadsheets.blade.php
@@ -2,40 +2,41 @@
 
 @section('content')
 
-<div class="row my-5">
-    <h3 class="fs-4 mb-3">Your Spreadsheets</h3>
-    <div class="col">
-        <table class="table bg-white rounded shadow-sm  table-hover">
-            <thead>
-                <tr>
-                    <th scope="col" width="50">#</th>
-                    <th scope="col">API NAME</th>
-                    <th scope="col">CREATED</th>
-                    <th scope="col">OPTION</th>
-                </tr>
-            </thead>
-            <tbody>
-            
-                @foreach ($spreads as $spread)
-                <tr>
+    <div class="row my-5">
+        <h3 class="fs-4 mb-3">Your Spreadsheets</h3>
+        <div class="col">
+            <table class="table bg-white rounded shadow-sm  table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col" width="50">#</th>
+                        <th scope="col">API NAME</th>
+                        <th scope="col">spread sheet link</th>
+                        <th scope="col">CREATED</th>
+                        <th scope="col">OPTION</th>
+                    </tr>
+                </thead>
+                <tbody>
 
-                    {{-- <form action="{{ route('categories.destroy', $cat->id) }}" method="POST" id="myForm"> --}}
+                    @foreach ($spreads as $spread)
+                        <tr>
 
-                    <th scope="row">{{ $spread->id}}</th>
-                    <td>{{ $spread->registration_number }}</td>
-                    <td>{{ $spread->created_at }}</td>
-                    <td>{{ $spread->spreadsheetID }}</td>
-                    <td>
-                        {{-- <button class="btn btn-success editbtn" value="{{ $spread->id }}">Edit</button> --}}
-                        {{-- @csrf --}}
-                        {{-- <a class="btn btn-danger" href="categorie/delete/{{ $spread->id }}"
+                            {{-- <form action="{{ route('categories.destroy', $cat->id) }}" method="POST" id="myForm"> --}}
+
+                            <th scope="row">{{ $spread->id }}</th>
+                            <td><a style="text-decoration: none; color: forestgreen;" target="_blanck" href="http://localhost:8000/api/v1/{{ $spread->registration_number }}">http://localhost:8000/api/v1/{{ $spread->registration_number }}</a></td>
+                            <td><a style="text-decoration: none; color: forestgreen;" target="_blanck" href="https://docs.google.com/spreadsheets/d/{{ $spread->spreadsheetID }}/edit#gid=0">https://docs.google.com/spreadsheets/d/{{ $spread->spreadsheetID }}/edit#gid=0</a></td>
+                            <td>{{ $spread->created_at }}</td>
+                            <td>
+                                {{-- <button class="btn btn-success editbtn" value="{{ $spread->id }}">Edit</button> --}}
+                                {{-- @csrf --}}
+                                {{-- <a class="btn btn-danger" href="categorie/delete/{{ $spread->id }}"
                             onclick="return confirm('are you sure you want to delete this Catrgory')">Delete</a> --}}
-                        {{-- <form> --}}
-                    </td>
-                </tr>
-            @endforeach
-            </tbody>
-        </table>
+                                {{-- <form> --}}
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
-</div>
 @endsection
